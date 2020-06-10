f3f0c6f09413e157aed246226d5ebc465d470b95
Kyle Escobar <kslrtips@gmail.com>
2020-06-10 17:35:06 -0400

    Add mapping collection getters

--- mapping/src/main/kotlin/io/rsbox/sparrow/mapping/Mappings.kt
+++ mapping/src/main/kotlin/io/rsbox/sparrow/mapping/Mappings.kt
@@ -1,6 +1,7 @@
 package io.rsbox.sparrow.mapping
 
 import io.rsbox.sparrow.asm.Class
+import io.rsbox.sparrow.asm.Field
 import io.rsbox.sparrow.asm.Method
 
 /**
@@ -26,4 +27,33 @@ class Mappings {
      * Method mappings.
      */
     val methodMappings = mutableListOf<Mapping<Method>>()
+
+    /**
+     * The field mappings.
+     */
+    val fieldMappings = mutableListOf<Mapping<Field>>()
+
+    /**
+     * Gets a [Class] that has been mapped to the target.
+     *
+     * @param target Class
+     * @return Class?
+     */
+    fun getClassMapping(target: Class): Class? = classMappings.firstOrNull { it.mapped?.id == target.id }?.mapped
+
+    /**
+     * Gets a [Method] that has been mapped to the target.
+     *
+     * @param target Method
+     * @return Method?
+     */
+    fun getMethodMapping(target: Method): Method? = methodMappings.firstOrNull { it.mapped?.id == target.id }?.mapped
+
+    /**
+     * Gets a [Field] that has been mapped to the target.
+     *
+     * @param target Field
+     * @return Field?
+     */
+    fun getFieldMapping(target: Field): Field? = fieldMappings.firstOrNull { it.mapped?.id == target.id }?.mapped
 }
