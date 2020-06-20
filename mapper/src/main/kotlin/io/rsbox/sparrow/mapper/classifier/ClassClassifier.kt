6d27fa457b25c9e8fa42a9045f2d77ff0df0492b
Kyle Escobar <kslrtips@gmail.com>
2020-06-20 16:37:37 -0400

    Add class type check classifier

--- mapper/src/main/kotlin/io/rsbox/sparrow/mapper/classifier/ClassClassifier.kt
+++ mapper/src/main/kotlin/io/rsbox/sparrow/mapper/classifier/ClassClassifier.kt
@@ -1,6 +1,8 @@
 package io.rsbox.sparrow.mapper.classifier
 
 import io.rsbox.sparrow.asm.Class
+import org.objectweb.asm.Opcodes.*
+import kotlin.math.pow
 
 /**
  * Copyright (c) 2020 RSBox
@@ -11,11 +13,26 @@ import io.rsbox.sparrow.asm.Class
  * @author Kyle Escobar
  */
 
+/**
+ * Responsible for classifying classes together based on traits.
+ */
 class ClassClassifier : AbstractClassifier<Class>() {
 
     override fun init() {
-
+        classTypeCheck.register("class type check", 20.0)
     }
 
-    com
+    companion object {
+        /**
+         * Class type check classifier.
+         */
+        private val classTypeCheck = classifier<Class> { source, target ->
+            val mask = ACC_ENUM or ACC_INTERFACE or ACC_ABSTRACT or ACC_ANNOTATION
+
+            val resultSource = source.access and mask
+            val resultTarget = target.access and mask
+
+            (1 - Integer.bitCount(resultSource.toDouble().pow(resultTarget).toInt()) / 4).toDouble()
+        }
+    }
 }
